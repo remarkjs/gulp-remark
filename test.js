1ec7645885ff5ab5fb3801f38a497a08ea0723aa
Denys Dovhan <email@denysdovhan.com>
2015-12-06 13:51:21 +0200

    Add tests.

--- test.js
+++ test.js
@@ -1,8 +1,45 @@
-import { equal } from 'assert';
+import { File, PluginError } from 'gulp-util';
+import { equal, throws } from 'assert';
+import es from 'event-stream';
 import gulpMdast from './index';
+import html from 'mdast-html';
 
-it('should gulpMdast', () =>
-  equal(gulpMdast('unicorns'), 'unicorns'));
+const fixture = new File({
+  path: 'fixture.txt',
+  contents: new Buffer(`_italic_, **bold**.`)
+});
 
-it('should gulpMdast invalid input', () =>
-  equal(gulpMdast(), undefined));
+it('should not do anything', done => {
+  const stream = gulpMdast({
+    emphasis: "*",
+    strong: "_"
+  });
+  stream.write(fixture);
+
+  stream.once('data', file => {
+    equal(file.relative, 'fixture.txt');
+    equal(file.contents.toString().trim(), `*italic*, __bold__.`);
+    done();
+  });
+});
+
+it('should use plugins', done => {
+  const stream = gulpMdast().use(html);
+  stream.write(fixture);
+
+  stream.once('data', file => {
+    equal(file.relative, 'fixture.txt');
+    equal(
+      file.contents.toString().trim(),
+      '<p><em>italic</em>, <strong>bold</strong>.</p>'
+    );
+    done();
+  });
+});
+
+it('should throw PluginError with streams', () => {
+  const stream = new File({
+    contents: es.readArray(['_italic_', '**bold**', '.'])
+  });
+  throws(() => { gulpMdast().write(stream) }, PluginError);
+});
