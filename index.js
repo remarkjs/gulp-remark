ffdb816f8921b4b9fc2252c369b1a9805a50b71d
Denys Dovhan <email@denysdovhan.com>
2015-12-06 13:51:06 +0200

    Implement base features.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-export default function gulpMdast(input) {
-  return input;
+import { PluginError } from 'gulp-util';
+import { obj } from 'through2';
+import mdast from 'mdast';
+
+import pkg from './package';
+
+const PLUGIN_NAME = pkg.name;
+
+export default function gulpMdast(options = {}) {
+  const M = mdast();
+
+  const plugin = obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+    if (file.isStream()) {
+      return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+    }
+    if (file.isBuffer()) {
+      file.contents = new Buffer(M.process(file.contents.toString(), options));
+    }
+    this.push(file);
+    cb(null, file);
+  });
+
+  plugin.use = (pls, opts) => {
+    M.use(pls, opts);
+    return plugin;
+  };
+
+  return plugin;
 };
