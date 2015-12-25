0944f27b72507b6d3dbde4269b3949903c386670
Titus Wormer <tituswormer@gmail.com>
2015-12-25 15:10:34 +0100

    Add rough draft of gulp-remark

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
-export default function gulpMdast(input) {
-  return input;
+'use strict';
+import through from 'through2';
+import convertVinylToVfile from 'convert-vinyl-to-vfile';
+import configure from 'remark/lib/cli/file-set-pipeline/configure';
+import transform from 'remark/lib/cli/file-set-pipeline/transform';
+import log from 'remark/lib/cli/file-set-pipeline/log';
+
+export default function gulpMdast() {
+  return through.obj(function (file, encoding, callback) {
+    let error = null;
+    let convertedFile;
+    let vFile;
+    let context;
+
+    if (!file || file.isNull()) {
+      this.push();
+      return callback();
+    }
+
+    vFile = convertVinylToVfile(file);
+
+    // Hidden-feature to enable mdast to rewrite files.
+    // Otherwise the CLI ignores you.
+    vFile.namespace('remark:cli').given = true;
+
+    // See for more options: remark/lib/cli/cli
+    // Probably also insert plugins here when given.
+    context = {
+      detectRC: true,
+      cwd: process.cwd(),
+      stdout: process.stdout,
+      stderr: process.stderr,
+      files: [
+        vFile
+      ]
+    };
+
+    // remarkignore(5) is not yet supported, as its use is inside
+    // remark/lib/cli/file-set-pipeline/file-system.
+
+    configure(context);
+
+    transform(context, function (err) {
+      if (err) {
+        callback(error);
+      } else {
+        log(context);
+
+        // Done! TODO: set `vFile.contents` back on the vinyl `file`.
+        callback(null, file);
+      }
+    })
+  }, function (callback) {
+    callback();
+  });
 };
