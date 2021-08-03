148daee13ca0da3b0e297f8d6dc4ad879cd36d6c
Titus Wormer <tituswormer@gmail.com>
2021-08-03 13:18:33 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -5,18 +5,18 @@ import test from 'tape'
 import html from 'remark-html'
 import {remark} from './index.js'
 
-var fixture = new Vinyl({
+const fixture = new Vinyl({
   path: 'fixture.txt',
   contents: Buffer.from('_italic_, **bold**.')
 })
 
-var commonmarkFixture = new Vinyl({
+const commonmarkFixture = new Vinyl({
   path: 'fixture-commonmark.txt',
   contents: Buffer.from('1) List in commonmark')
 })
 
-test('gulp-remark', function (t) {
-  t.test('should not do anything', function (st) {
+test('gulp-remark', (t) => {
+  t.test('should not do anything', (st) => {
     st.plan(2)
 
     const stream = remark({silent: true})
@@ -30,8 +30,10 @@ test('gulp-remark', function (t) {
     }
   })
 
-  t.test('should support settings', function (st) {
-    var stream = remark({settings: {commonmark: true}, silent: true}).use(html)
+  t.test('should support settings', (st) => {
+    const stream = remark({settings: {commonmark: true}, silent: true}).use(
+      html
+    )
 
     st.plan(2)
 
@@ -48,8 +50,8 @@ test('gulp-remark', function (t) {
     }
   })
 
-  t.test('should use plugins', function (st) {
-    var stream = remark({silent: true}).use(html)
+  t.test('should use plugins', (st) => {
+    const stream = remark({silent: true}).use(html)
 
     st.plan(2)
 
@@ -66,10 +68,10 @@ test('gulp-remark', function (t) {
     }
   })
 
-  t.test('should throw PluginError with streams', function (st) {
-    var inStream = new PassThrough()
+  t.test('should throw PluginError with streams', (st) => {
+    const inStream = new PassThrough()
 
-    var outStream = new Vinyl({contents: inStream})
+    const outStream = new Vinyl({contents: inStream})
 
     st.throws(write, PluginError)
 
