3920f2bf489cc5e7ff4d2fc6a2278b0c240859d8
Titus Wormer <tituswormer@gmail.com>
2021-11-16 20:17:44 +0100

    Update dev-dependencies

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import {gulpEngine} from 'unified-engine-gulp'
 import {remark as processor} from 'remark'
 
 export const remark = gulpEngine({
-  // @ts-expect-error: fine.
   processor,
   name: 'gulp-remark',
   pluginPrefix: 'remark',
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+import {Buffer} from 'node:buffer'
 import {PassThrough} from 'node:stream'
 import PluginError from 'plugin-error'
 import Vinyl from 'vinyl'
